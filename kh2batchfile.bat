96a2dafd97f9b584164cca5c24d6bfba82b5b693
roromaniac <roko@umich.edu>
2022-04-27 11:08:11 -0400

    bat file

---
+++ kh2batchfile.bat
@@ -0,0 +1,14 @@
+@echo off
+cd (INSERT FOLDER THAT HAS KH TRACKER HERE)
+start KhTracker.exe 
+cd (INSERT FOLDER THAT HAS KH2 Randomizer exe HERE)
+start KH2Randomizer.exe 
+cd (INSERT OPENKH FOLDER PATH HERE)
+start OpenKh.Tools.ModsManager.exe 
+cd (INSERT BUILD FROM MM FOLDER HERE)
+start build_from_mm.exe 
+cd (INSERT FOLDER THAT HAS LIVESPLIT)
+start LiveSplit.exe 
+cd (INSERT FOLDER THAT HAS EPIC GAMES)\Epic Games\Launcher\Portal\Binaries\Win32
+start EpicGamesLauncher.exe
+exit
